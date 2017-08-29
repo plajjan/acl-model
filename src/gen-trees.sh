23301006432f6a756922c930f21d6e79fdb961df
Mahesh Jethanandani <mjethanandani@gmail.com>
2017-08-29 13:25:53 -0700

    Updated draft after IETF 99.

--- src/gen-trees.sh
+++ src/gen-trees.sh
@@ -1,8 +1,12 @@
-for i in yang/ietf-*\@$(date +%Y-%m-%d).yang
+for i in yang/*\@$(date +%Y-%m-%d).yang
 do
 	name=$(echo $i | cut -f 1 -d '.')
 	echo $name
-	pyang -p ../ -f tree $name.yang > $name-tree.txt.tmp
+        if test "${name#^example}" = "$name"; then
+        	pyang -p ../ -f tree --tree-line-length=72 $name.yang > $name-tree.txt.tmp
+        else            
+        	pyang --ietf -p ../ -f tree --tree-line-length=72 $name.yang > $name-tree.txt.tmp
+        fi
 	fold -w 71 $name-tree.txt.tmp > $name-tree.txt
 done
 rm yang/*-tree.txt.tmp
